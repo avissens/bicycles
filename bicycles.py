9984afb95f2888c9ce7a2c4db2c15bd6912b334b
avissens <aviss@hotmail.co.uk>
2016-10-26 12:36:26 +0000

    Adding the final version with self.profit returned

--- bicycles.py
+++ bicycles.py
@@ -5,7 +5,7 @@ class Bicycle(object): #new class of bike
         self.model = model
         self.weight = weight
         self.cost = cost
-       
+        
 class Shop(object): #new class of shops
     margin = 20
     def __init__(self, shop_name, inventory, expenses):
@@ -19,8 +19,44 @@ class Shop(object): #new class of shops
         for item in self.inventory:
             price = bike.cost + (bike.cost*self.margin)/100
         return int(price)
+
+    def calc_profit(self): #calculating profit
+        sell_profit = 0
+        for item in self.sold_bikes:
+            sell_profit = int((item.cost*self.margin)/100 - self.expenses)
+            self.profit += sell_profit
+        print("Shop's profit is $" + str(self.profit))
+        return self.profit
         
+    def display(self):
+        print(self.shop_name + "'s inventory: ")
+        for item in self.inventory:
+            print(item.model + ": " + "weight(gr) - " + str(item.weight) + ", price($) - " + str(self.price(item)))
+
 class Customer(object): #new class of customers
     def __init__(self, name, budget):
         self.name = name
         self.budget = budget
+        
+    def offer(self, shop):
+        offered_bikes = []
+        for item in shop.inventory:
+            if self.budget >= shop.price(item):
+                offered_bikes.append(item.model)
+        print(self.name + ", this is the choice you have: " + ", ".join(offered_bikes))
+        return offered_bikes
+        
+    def sell(self, offered_bikes, shop):
+        choice = random.choice(offered_bikes)
+        print("Ok, I'll go for this one: " + choice)
+        for item in shop.inventory:
+            if choice == item.model:
+                self.budget -= shop.price(item)
+                shop.inventory.remove(item)
+                shop.sold_bikes.append(item)
+        return self.budget        
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from bicycles import Bicycle, Shop, Customer
-import random
+from bicycles_v2 import Bicycle, Shop, Customer
 
 myvelo = Bicycle("myvelo-333", 500, 120) #bike instance 1
 panda = Bicycle("panda-444", 400, 350) #bike instance 2
@@ -17,44 +16,11 @@ ruben = Customer("Ruben", 500) #customer instance 2
 maxim = Customer("Maxim", 1000) #customer instance 3
 customers = [anna, ruben, maxim]
 
-
-print(my_shop.shop_name + "'s inventory: ")
-for item in my_shop.inventory:
-    print(item.model + ": " + "weight(gr) - " + str(item.weight) + ", price($) - " + str(my_shop.price(item)))
-
-def offer(self):
-        offered_bikes = []
-        for item in my_shop.inventory:
-            if self.budget >= my_shop.price(item):
-                offered_bikes.append(item.model)
-        print(self.name + ", this is the choice you have: " + ", ".join(offered_bikes))
-        return offered_bikes
-      
-def sell(self, offered_bikes):
-        choice = random.choice(offered_bikes)
-        budget_left = 0
-        print("Ok, I'll go for this one: " + choice)
-        for bike in my_shop.inventory:
-            if choice == bike.model:
-                budget_left = self.budget - my_shop.price(bike)
-                my_shop.inventory.remove(bike)
-                my_shop.sold_bikes.append(bike)
-        return budget_left
-        
-for customer in customers:
-    offered_bikes = offer(customer)
-    budget_left = sell(customer, offered_bikes)
-    print(customer.name + ", enjoy your new bike! You still have $" + str(budget_left) + " left in your budget. Fancy some accessories?")
-
-print(my_shop.shop_name + "'s inventory: ")
-for item in my_shop.inventory:
-    print(item.model + ": " + "weight(gr) - " + str(item.weight) + ", price($) - " + str(my_shop.price(item)))
-
-def calc_profit(self): #calculating profit
-        sell_profit = 0
-        for bike in self.sold_bikes:
-            sell_profit = int((bike.cost*self.margin)/100 - self.expenses)
-            self.profit += sell_profit
-        return self.profit
-
-print("Shop's profit is $" + str(calc_profit(my_shop)))
+if __name__ == "__main__":
+    my_shop.display()
+    for customer in customers:
+        offered_bikes = customer.offer(my_shop)
+        budget_left = customer.sell(offered_bikes, my_shop)
+        print(customer.name + ", enjoy your new bike! You still have $" + str(customer.budget) + " left in your budget. Fancy some accessories?")
+    my_shop.display()
+    my_shop.calc_profit()
